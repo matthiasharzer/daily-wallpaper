a80c152eda4a2f8449d467559213faff40d5d17a
Matthias Harzer <matthias.harzer03@gmail.com>
2026-08-04 22:43:19 +0200

    feat: add local config and storage persisting

---
+++ local/storage_directory.go
@@ -0,0 +1,26 @@
+package local
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const appName = "daily-wallpaper"
+
+var StorageDirectory string
+
+func init() {
+	var err error
+
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		panic(fmt.Errorf("failed to get user config dir: %w", err))
+	}
+
+	StorageDirectory = filepath.Join(configDir, appName)
+	err = os.MkdirAll(StorageDirectory, 0755)
+	if err != nil {
+		panic(fmt.Errorf("failed to create storage directory: %w", err))
+	}
+}
